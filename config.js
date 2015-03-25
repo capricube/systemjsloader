27ed7538c57509b731302b53ae484432f371ff1b
Kristian Marinkovic <kristian.marinkovic@at.redbullmediahouse.com>
2015-03-25 15:17:06 +0100

    more documentation

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ System.config({
       "process": "npm:process@0.10.1"
     },
     "npm:backbone@1.1.2": {
-      "process": "github:jspm/nodelibs-process@0.1.1",
+     "process": "github:jspm/nodelibs-process@0.1.1", 
       "underscore": "npm:underscore@1.8.2"
     }
   }
