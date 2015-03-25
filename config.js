fde6c599f46f9a58d6dc0f3228ef1a2a2eee2a3e
Kristian Marinkovic <kristian.marinkovic@at.redbullmediahouse.com>
2015-03-25 16:19:45 +0100

    there are essentially two ways to add missing dependency information one using a shim with deps or declare the missing dendencies using the resolver string

--- config.js
+++ config.js
@@ -3,15 +3,6 @@ System.config({
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js"
-  },
-  "shim": {
-    "marionette": {
-      "deps": [
-        "underscore",
-        "backbone.wreqr",
-        "backbone.babysitter"
-      ]
-    }
   }
 });
 
@@ -22,12 +13,20 @@ System.config({
     "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
     "jquery": "github:components/jquery@2.1.3",
     "marionette": "github:marionettejs/backbone.marionette@2.4.1",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
     "underscore": "npm:underscore@1.8.2",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
+    "github:marionettejs/backbone.marionette@2.4.1": {
+      "backbone": "npm:backbone@1.1.2",
+      "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
+      "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
+      "underscore": "npm:underscore@1.8.2"
+    },
     "npm:backbone@1.1.2": {
-     "process": "github:jspm/nodelibs-process@0.1.1", 
+      "process": "github:jspm/nodelibs-process@0.1.1",
       "underscore": "npm:underscore@1.8.2"
     }
   }
