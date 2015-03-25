377fd2199cdd823d9b64f4a08f7cd3774d25044c
Kristian Marinkovic <kristian.marinkovic@at.redbullmediahouse.com>
2015-03-25 13:53:18 +0100

    initial commit for a really simple system.js example following the doc on https://github.com/jspm/jspm-cli/wiki/Getting-Started

---
+++ app-amd.js
@@ -0,0 +1,5 @@
+"use strict";
+
+define(['jquery'], function($) {
+    console.log("jquery loaded");
+});
---
+++ config.js
@@ -0,0 +1,23 @@
+System.config({
+  "baseURL": "/",
+  "transpiler": "babel",
+  "babelOptions": {
+    "optional": [
+      "runtime"
+    ]
+  },
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js",
+    "npm:*": "jspm_packages/npm/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "babel": "npm:babel@4.7.16",
+    "babel-runtime": "npm:babel-runtime@4.7.16",
+    "jquery": "github:components/jquery@2.1.3"
+  }
+});
+
