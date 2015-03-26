347a637be6024efe6fa4adeb3b06e8333bf4282c
Kristian Marinkovic <kristian.marinkovic@at.redbullmediahouse.com>
2015-03-26 13:52:12 +0100

    you need bootstrap.js only to set the jquery dependency if you your common.js modules. If you use the github: version it will be able to resolve the dependency as AMD module. npm: dependencies are always resolved as commen js modules!

--- config.js
+++ config.js
@@ -1,34 +1,40 @@
 System.config({
-  "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
-  }
+    "paths": {
+        "*": "*.js",
+        "github:*": "jspm_packages/github/*.js"
+    },
+    "meta": {
+        "backbone": {
+            format: "amd"
+        },
+        "jquery": {
+            format: "amd"
+        },
+        "underscore": {
+            format: "amd"
+        },
+        "marionette": {
+            format: "amd"
+        },
+        "backbone.wreqr": {
+            format: "amd"
+        },
+        "backbone.babysitter": {
+            format: "amd"
+        }
+    }
 });
+
 
 System.config({
-  "map": {
-    "backbone": "npm:backbone@1.1.2",
-    "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
-    "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
-    "jquery": "github:components/jquery@2.1.3",
-    "marionette": "github:marionettejs/backbone.marionette@2.4.1",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
-    "underscore": "npm:underscore@1.8.2",
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "github:marionettejs/backbone.marionette@2.4.1": {
-      "backbone": "npm:backbone@1.1.2",
-      "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
-      "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
-      "underscore": "npm:underscore@1.8.2"
-    },
-    "npm:backbone@1.1.2": {
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "underscore": "npm:underscore@1.8.2"
+    "map": {
+        "backbone": "github:jashkenas/backbone@1.1.2",
+        "backbone.babysitter": "github:marionettejs/backbone.babysitter@0.1.6",
+        "backbone.wreqr": "github:marionettejs/backbone.wreqr@1.3.2",
+        "jquery": "github:components/jquery@2.1.3",
+        "marionette": "github:marionettejs/backbone.marionette@2.4.1",
+        "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+        "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87",
+        "underscore": "github:jashkenas/underscore@1.8.2"
     }
-  }
 });
-
